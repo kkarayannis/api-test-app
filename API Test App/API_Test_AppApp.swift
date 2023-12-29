66ea651fc949aa3a11cd39d25e5d9c52bb2d9de0
Kostas Karayannis <kkarayannis@gmail.com>
2023-12-29 09:29:45 +0100

    Initial Commit

---
+++ API Test App/API_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Test_AppApp.swift
+//  API Test App
+//
+//  Created by Konstadinos Karayannis on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct API_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
